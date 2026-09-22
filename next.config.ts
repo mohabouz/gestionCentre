4f2a28f81a7f9b11507a9e43488dfa8a4b339bac
mohabouz <bouzidmohamed7@gmail.com>
2026-09-22 13:56:09 +0100

    Remove Next.js output and image optimization settings
    Removed output configuration and image optimization settings.

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // Tells Next.js to generate static HTML/CSS/JS
-  images: {
-    unoptimized: true, // GitHub Pages doesn't support Next.js default image optimization
-  },
 };
 
 export default nextConfig;
